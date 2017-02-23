7fb7355be79addec028be8948e6c62ae8ec0c748
hack <hacktavist616@gmail.com>
2017-02-22 20:18:47 -0500

    mockup to get data from gmail

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'gmail'
+class Hak5Notifier < Sinatra::Base
+
+	gmail = Gmail.connect(ENV["UNAME"], ENV["PW"])
+
+	get '/' do 
+		@prev_unread = 0
+		while @prev_unread == 0 do
+			#get the number of unread messages in the inbox
+			unread = gmail.mailbox('Hak5 Videos').count(:unread)
+
+			if unread > @prev_unread
+			    @newHak5Videos = unread
+			end
+
+			  #reset the number of unread emails
+			  @prev_unread = unread
+
+
+		end
+		erb :'unread'
+	end
+
+
+
+end
